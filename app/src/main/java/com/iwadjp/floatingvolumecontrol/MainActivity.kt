78c23d66880c4d8f1b6771858f52a2cf242859b6
diwasa1 <daisuke.iwasa@unisys.co.jp>
2020-05-05 12:00:50 +0900

    オーバーレイのサンプル実装中

--- app/src/main/java/com/iwadjp/floatingvolumecontrol/MainActivity.kt
+++ app/src/main/java/com/iwadjp/floatingvolumecontrol/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.iwadjp.floatingvolumecontrol
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,7 +24,35 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         if (hasOverlayPermission()) {
-
+            val intent = Intent(this, MyVolumeControlService::class.java)
+                .setAction(MyVolumeControlService.ACTION_STOP)  // TODO: STOP と STARTは合っている？
+            startService(intent)
+        } else {
+            requestOverlayPermission(REQUEST_OVERLAY_PERMISSION)
         }
     }
+
+    override fun onStop() {
+        super.onStop()
+        if (enable && hasOverlayPermission()) {
+            val intent = Intent(this, MyVolumeControlService::class.java)
+                .setAction(MyVolumeControlService.ACTION_START)
+            startService(intent)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            when(requestCode) {
+                REQUEST_OVERLAY_PERMISSION -> Log.d(TAG, "enable overlay permisson")
+            }
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        enable = false
+        return super.onTouchEvent(event)
+    }
+
 }
