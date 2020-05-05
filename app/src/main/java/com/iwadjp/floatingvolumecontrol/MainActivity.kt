9e9f65af5f3b58fc73e3719641713131224b0b91
diwasa1 <daisuke.iwasa@unisys.co.jp>
2020-05-05 11:49:17 +0900

    オーバーレイのサンプル実装中

--- app/src/main/java/com/iwadjp/floatingvolumecontrol/MainActivity.kt
+++ app/src/main/java/com/iwadjp/floatingvolumecontrol/MainActivity.kt
@@ -5,8 +5,22 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        private val TAG = MainActivity::class.qualifiedName
+        private val REQUEST_OVERLAY_PERMISSION = 1
+    }
+
+    private var enable = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (hasOverlayPermission()) {
+
+        }
     }
 }
