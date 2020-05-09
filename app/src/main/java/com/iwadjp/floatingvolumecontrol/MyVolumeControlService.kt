7e2522db6406f5e21be65eaad3ad0f6847e7db44
diwasa1 <daisuke.iwasa@unisys.co.jp>
2020-05-09 09:45:11 +0900

    リファクタリング

--- app/src/main/java/com/iwadjp/floatingvolumecontrol/MyVolumeControlService.kt
+++ app/src/main/java/com/iwadjp/floatingvolumecontrol/MyVolumeControlService.kt
@@ -5,8 +5,6 @@ import android.app.Notification
 import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
-import android.content.Context
-import android.icu.text.Transliterator
 import android.os.Build
 import android.os.IBinder
 import android.view.WindowManager
@@ -14,22 +12,11 @@ import android.widget.ImageView
 import androidx.annotation.RequiresApi
 import kotlin.random.Random
 
-// TODO: Rename actions, choose action names that describe tasks that this
-// IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
-private const val ACTION_FOO = "com.iwadjp.floatingvolumecontrol.action.FOO"
-private const val ACTION_BAZ = "com.iwadjp.floatingvolumecontrol.action.BAZ"
-
-// TODO: Rename parameters
-private const val EXTRA_PARAM1 = "com.iwadjp.floatingvolumecontrol.extra.PARAM1"
-private const val EXTRA_PARAM2 = "com.iwadjp.floatingvolumecontrol.extra.PARAM2"
-
 /**
  * An [IntentService] subclass for handling asynchronous task requests in
  * a service on a separate handler thread.
- * TODO: Customize class - update intent actions, extra parameters and static
- * helper methods.
  */
-class MyVolumeControlService : IntentService("MyVolumeControlService") {
+class MyVolumeControlService : IntentService("MyVolumeControlService") {    // Todo: IntentService は非推奨
 
     private val notificationID = Random.nextInt()
 
@@ -48,7 +35,7 @@ class MyVolumeControlService : IntentService("MyVolumeControlService") {
         val notification = Notification.Builder(this, MainActivity.CHANNEL_ID)
             .setContentIntent(pendingIntent)
             .setSmallIcon(R.mipmap.ic_launcher)
-            .setContentTitle("Service is running.")
+            .setContentTitle(getString(R.string.notification_title))
             .build()
         startForeground(notificationID, notification)
     }
@@ -70,7 +57,7 @@ class MyVolumeControlService : IntentService("MyVolumeControlService") {
             val windowManager = getSystemService(Service.WINDOW_SERVICE) as WindowManager
             setImageResource(android.R.drawable.ic_menu_add)
             button = FloatingButton(windowManager, this, context).apply {
-                setPosition(context)
+                setPosition(context)    // ToDo: このやり方が適切
                 visible = true
             }
         }
@@ -89,72 +76,10 @@ class MyVolumeControlService : IntentService("MyVolumeControlService") {
     }
 
     override fun onHandleIntent(intent: Intent?) {
-        when (intent?.action) {
-            ACTION_FOO -> {
-                val param1 = intent.getStringExtra(EXTRA_PARAM1)
-                val param2 = intent.getStringExtra(EXTRA_PARAM2)
-                handleActionFoo(param1, param2)
-            }
-            ACTION_BAZ -> {
-                val param1 = intent.getStringExtra(EXTRA_PARAM1)
-                val param2 = intent.getStringExtra(EXTRA_PARAM2)
-                handleActionBaz(param1, param2)
-            }
-        }
-    }
-
-    /**
-     * Handle action Foo in the provided background thread with the provided
-     * parameters.
-     */
-    private fun handleActionFoo(param1: String, param2: String) {
-        TODO("Handle action Foo")
-    }
-
-    /**
-     * Handle action Baz in the provided background thread with the provided
-     * parameters.
-     */
-    private fun handleActionBaz(param1: String, param2: String) {
-        TODO("Handle action Baz")
     }
 
     companion object {
         val ACTION_START = "start"
         val ACTION_STOP = "stop"
-
-        /**
-         * Starts this service to perform action Foo with the given parameters. If
-         * the service is already performing a task this action will be queued.
-         *
-         * @see IntentService
-         */
-        // TODO: Customize helper method
-        @JvmStatic
-        fun startActionFoo(context: Context, param1: String, param2: String) {
-            val intent = Intent(context, MyVolumeControlService::class.java).apply {
-                action = ACTION_FOO
-                putExtra(EXTRA_PARAM1, param1)
-                putExtra(EXTRA_PARAM2, param2)
-            }
-            context.startService(intent)
-        }
-
-        /**
-         * Starts this service to perform action Baz with the given parameters. If
-         * the service is already performing a task this action will be queued.
-         *
-         * @see IntentService
-         */
-        // TODO: Customize helper method
-        @JvmStatic
-        fun startActionBaz(context: Context, param1: String, param2: String) {
-            val intent = Intent(context, MyVolumeControlService::class.java).apply {
-                action = ACTION_BAZ
-                putExtra(EXTRA_PARAM1, param1)
-                putExtra(EXTRA_PARAM2, param2)
-            }
-            context.startService(intent)
-        }
     }
 }
